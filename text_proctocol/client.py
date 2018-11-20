24122bb4221181ebdcbb022c4062f39e7a303527
Mateusz <matostr98@gmail.com>
2018-11-20 13:40:17 +0100

    UDP Init

--- text_proctocol/client.py
+++ text_proctocol/client.py
@@ -2,3 +2,27 @@ print("""
 ########################
 #       CLIENT         #
 ########################""")
+
+import socket
+import sys
+import segment
+from bitstring import BitArray
+
+if (sys.argv[1] == 'm'):
+    HOST = '192.168.0.20'
+else:
+    HOST = '127.0.0.1'
+
+#HOST = '192.168.43.57'
+
+PORT = 4666
+send = "PUTElita"
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    #s.bind(('', PORT))
+    s.sendto(send.encode('ascii'), (HOST, PORT))
+    data = s.recvfrom(1024)
+    print(f"Server reply: {data.decode('ascii')}")
+    
+print("Recieved", repr(data))    
+
--- text_proctocol/server.py
+++ text_proctocol/server.py
@@ -2,3 +2,19 @@ print("""
 ########################
 #       SERVER         #
 ########################""")
+
+import socket
+
+HOST = '127.0.0.1'
+#HOST = '192.168.43.102'
+#HOST = '192.168.0.21'
+
+PORT = 6666
+msg = "Stupid Together"
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    data = s.recvfrom(1024)
+    d = data[0]
+    clientAddr = data[1]
+    s.sendto(msg, clientAddr)
+
