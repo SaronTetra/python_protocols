85018171e2cc41b9052240811fd4464b6cc819da
Maciej S <starwars.maciek@gmail.com>
2018-11-20 14:40:31 +0100

    Added: basic connection

--- text_proctocol/client.py
+++ text_proctocol/client.py
@@ -5,24 +5,21 @@ print("""
 
 import socket
 import sys
-import segment
-from bitstring import BitArray
 
-if (sys.argv[1] == 'm'):
-    HOST = '192.168.0.20'
-else:
-    HOST = '127.0.0.1'
+# if (sys.argv[1] == 'm'):
+#     HOST = '192.168.0.20'
+# else:
+HOST = '127.0.0.1'
 
-#HOST = '192.168.43.57'
-
-PORT = 4666
+PORT = 6666
 send = "PUTElita"
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    #s.bind(('', PORT))
-    s.sendto(send.encode('ascii'), (HOST, PORT))
-    data = s.recvfrom(1024)
-    print(f"Server reply: {data.decode('ascii')}")
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(send.encode('ascii'), (HOST, PORT))
+data, server = s.recvfrom(1024)
+print(f"Server reply: {data}")
     
-print("Recieved", repr(data))    
+print("Recieved", repr(data))
+s.close()
+
 
--- text_proctocol/server.py
+++ text_proctocol/server.py
@@ -6,15 +6,18 @@ print("""
 import socket
 
 HOST = '127.0.0.1'
-#HOST = '192.168.43.102'
-#HOST = '192.168.0.21'
+#HOST = '192.168.0.20'
 
 PORT = 6666
 msg = "Stupid Together"
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    data = s.recvfrom(1024)
-    d = data[0]
-    clientAddr = data[1]
-    s.sendto(msg, clientAddr)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((HOST, PORT))
+while True:
+    data, addres = s.recvfrom(1024)
+    if data:
+        print("got data", data)
+# d = data[0]
+# clientAddr = data[1]
+    s.sendto(msg.encode("ascii"), addres)
 
